8952cc00803259c9918ac205077a394bd5afc868
devmahesh486 <97374063+devmahesh486@users.noreply.github.com>
2022-03-22 22:25:30 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "KIA6HCXMEU4GH4FZN4U"
-  }
-variable "secret_key" {
-  default = "iyhSgNJxEuCGa847+qbJ9z3pE8KD+SvHjRijI42U"
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
