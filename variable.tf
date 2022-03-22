69d9b9309b00561e1dcb939fb1f44f8041cce364
devmahesh486 <97374063+devmahesh486@users.noreply.github.com>
2022-03-22 22:25:46 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "KIA6HCXMEU4GH4FZN4U"
+  }
+variable "secret_key" {
+  default = "iyhSgNJxEuCGa847+qbJ9z3pE8KD+SvHjRijI42U"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
